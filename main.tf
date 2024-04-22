6d597d46790071c39f1c67bf42c67912514203f7
Abd Elrahman Ali Heggo <a.heggo.work@gmail.com>
2024-04-22 15:02:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "my_vm" {
  ami           = var.ami //Ubuntu AMI
  instance_type = var.instance_type
+ vpc_security_group_ids = [aws_security_group.task_sg.id]
 
  tags = {
    Name = var.name_tag,
@@ -10,4 +11,40 @@ resource "local_file" "public_ip_file"{
    filename = "ansible/inventory"
    content = aws_instance.my_vm.public_ip
 }
+
+resource "aws_security_group" "task_sg" {
+  name        = "ec2-security-group"
+  description = "Security group for example instance"
+
+  // Ingress rule for SSH (port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access from anywhere
+  }
+
+  // Ingress rule for ICMP (ping)
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow ping from anywhere
+  }
+ // Ingress rule for HTTP (port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP access from anywhere
+  }
+  // Egress rule allowing all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
